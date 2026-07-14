5db0bd01c2289da4ba07cb19ec431b5d7fd87114
changch <changch@MIS_3420>
2026-07-14 12:24:25 +0800

    add tfvc

--- init.lua
+++ init.lua
@@ -240,6 +240,8 @@ do
 
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+  vim.keymap.set('n', '<leader>vp', function() require('custom.tfvc_pending').open() end, { desc = 'TFVC Pending Changes' })
+
   -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
   -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
   -- is not what someone will guess without a bit more experience.
